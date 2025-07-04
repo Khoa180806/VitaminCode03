af278ee00a5c060bbd45fca32b3c75f51d3664fa
Khoa180806 <khoaphan180806@gmail.com>
2025-07-04 21:43:05 +0700

    Add Department

---
+++ Entity/Department.java
@@ -0,0 +1,35 @@
+package Entity;
+
+import java.util.*;
+
+public class Department {
+    private String departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private List<Employee> employees;
+
+    public Department(String departmentId, String departmentName, String departmentAddress) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.departmentAddress = departmentAddress;
+        this.employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void showListEmployee() {
+        for (Employee employee : employees) {
+            employee.showInfo();
+        }
+    }
+
+    public void sumSalary() {
+        double sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.calculateSalary();
+        }
+        System.out.println("Sum salary: " + sum);
+    }
+}
